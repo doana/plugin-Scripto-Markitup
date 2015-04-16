8273b23c8146c7b75ed1cbfadf910f0087dfafa4
Adam Doan <doana@uoguelph.ca>
2015-04-16 16:24:29 -0400

    Now inserts two newline characters for each 'enter' keypress.

--- views/shared/markitup/scripto-markitup.js
+++ views/shared/markitup/scripto-markitup.js
@@ -1,3 +1,14 @@
 jQuery( window ).load(function() {
-   jQuery('#scripto-transcription-page-wikitext').markItUp(mySettings);
-}); 
+    jQuery('#scripto-transcription-page-wikitext').markItUp(mySettings);
+    jQuery('#scripto-transcription textarea').keypress(function(event) {
+        if(event.keyCode == 13) {
+            insertAtCaret(this, "\n");
+        }
+    });
+});
+
+function insertAtCaret(element, text) {
+    var caretPos = jQuery(element).prop("selectionStart");
+    var currentValue = jQuery(element).val();
+    jQuery(element).val(currentValue.substring(0, caretPos) + text + currentValue.substring(caretPos));
+}
